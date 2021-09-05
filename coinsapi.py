078dfd63431b151b19677452362865aba209c832
Maciej Slawny <maciekslawny@gmail.com>
2021-09-05 10:31:31 +0200

    update

--- coinsapi.py
+++ coinsapi.py
@@ -5,15 +5,15 @@ import threading
 AMOUNT_OF_COINS = 25 
 
 def load_api(): 
+    """Wczytywanie danych z API powtarzane co 60 SEC"""
     threading.Timer(60.0, load_api).start() # called every minute
     global url, all_coins_data 
     url = f'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page={AMOUNT_OF_COINS}&page=1&sparkline=false'
     all_coins_data = loads(get(url).text)
 
 
-
-
 def create_coin(coin_input):
+    """Zwraca informację o wybranych kryptowalutach"""
     coins_list = []
     if(coin_input == 'all'):
         for coin in all_coins_data:
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from coinsapi import *
 from flask import Flask, redirect, url_for, render_template
 
-
 app = Flask(__name__)
 app.secret_key = "hello"
 
@@ -9,12 +8,9 @@ load_api()
 
 coins_data = create_coin('all')
 
-
 @app.route("/")
 def main():
     return render_template("index.html", coins_data = coins_data)
-
-
 
 @app.route("/<coin>")
 def coin_page(coin):
